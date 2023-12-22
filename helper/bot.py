11f16c6e95a33e17b4dc15fd793b2d710363118d
Johann Berger <johann@objekt.stream>
2023-12-22 16:27:03 +0100

    ~ update channel

--- helper/bot.py
+++ helper/bot.py
@@ -1,7 +1,7 @@
 import requests
 from helper.config import DISCORD_TOKEN
 
-CHANNEL_ID = '1187741455876366387'
+CHANNEL_ID = '1187777936657481728'
 
 def send_file(path: str, content: str = ''):
     with open(path, 'rb') as f:
