2d943d482d1eac4184a4e471357a53addd96686b
Johann Berger <johann@objekt.stream>
2023-12-22 16:22:46 +0100

    ~fix

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from helper.popular import get_popular_videos
 from helper.bot import send_file
 from helper.db import get_json, set_json
 
-uploaded_videos = get_json()
 videos = [v
     for v in get_popular_videos() 
-        if v not in uploaded_videos
+        if v not in get_json()
 ]
 
 for video in videos:
@@ -15,4 +14,4 @@ for video in videos:
         print("uploading", clip)
         send_file(clip, f"[source]({video})")
         
-    set_json(uploaded_videos + [video])
+    set_json(get_json() + [video])
