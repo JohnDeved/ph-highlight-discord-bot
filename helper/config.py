3f97150249f1225e612c731af7454e4544dffd30
undefined <johann@objekt.stream>
2023-12-23 02:41:33 +0100

    ~ tab size

--- helper/config.py
+++ helper/config.py
@@ -4,8 +4,8 @@ load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "")
 if not DISCORD_TOKEN:
-  raise ValueError("DISCORD_TOKEN is not set in the environment.")
+    raise ValueError("DISCORD_TOKEN is not set in the environment.")
 
 GH_TOKEN = os.getenv("GH_TOKEN", "")
 if not GH_TOKEN:
-  raise ValueError("GH_TOKEN is not set in the environment.")
+    raise ValueError("GH_TOKEN is not set in the environment.")
